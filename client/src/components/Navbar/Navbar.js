bbc2017ce3ae15565c7325185b068b9e51718c6b
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-04-23 18:49:21 +0100

    update

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import { Button } from '../Button'
 import {MenuItems} from "./MenuItems"
 import "./Navbar.css"
 
 class Navbar extends Component {
+    state ={
+        clicked:false
+    }
+
+    handleClick=()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
     render() {
         return (
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? "nav-menu active": "nav-menu"}>
                     {MenuItems.map((items,index)=>{
                         return (
                             <li key={index}>
@@ -19,8 +28,8 @@ class Navbar extends Component {
                             </li>
                         )
                     })}
-
                 </ul>
+                <Button>Sign Up</Button>
             </nav>
         )
     }
