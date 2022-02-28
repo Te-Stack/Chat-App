04f326955c00b8a7d3b2cd7147dbcda4268b94f9
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-02-28 17:13:46 +0100

    Working

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -4,8 +4,15 @@ import HeartFilled from "../../img/heartFilled.svg"
 import Comment from "../../img/comment.svg"
 import Share from "../../img/share.svg"
 import Info from "../../img/info.svg"
+import { useState } from "react"
 
 const Card = ({post}) => {
+    const [liked, setLiked] = useState(false);
+
+    const handleNotification=()=>{
+        setLiked(true)
+    }
+
     return ( 
         <div className="card">
             <div className="info">
@@ -14,7 +21,9 @@ const Card = ({post}) => {
             </div>
             <img src={post.postImg} alt="" className="postImg" />
             <div className="interaction">
-                <img src={Heart} alt="" className="cardIcon" />
+                {liked ? (<img src={HeartFilled} alt="" className="cardIcon" />): (
+                    <img src={Heart} alt="" className="cardIcon" onClick={handleNotification} />
+                )}
                 <img src={Comment} alt="" className="cardIcon" />
                 <img src={Share} alt="" className="cardIcon" />
                 <img src={Info} alt="" className="cardIcon infoIcon" />
