72347056fff160ebbde88030d3fdf49ecbc61eb4
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-02-28 15:50:44 +0100

    Working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Navbar from './components/navbar/Navbar'
 import Card from './components/card/Card'
 import './index.css';
+import {posts} from "./data"
 
 const App = () => {
   const [username,setUsername] = useState("")
@@ -13,7 +14,10 @@ const App = () => {
       {user ? (
         <>
         <Navbar/>
-        <Card/>
+        {posts.map((post)=>(
+          <Card key={post.id} post={post} />
+        ))}
+        
         <span className="username">
           {user}
         </span>
