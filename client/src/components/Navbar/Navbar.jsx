7165df04a00480439d0ed16a3404a12f028d391a
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-03-08 13:09:43 +0100

    Working

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -44,20 +44,21 @@ const Navbar = ({socket}) => {
             <div className="icons">
                 <div className="icon" onClick={()=>setOpen(!open)}>
                     <img src={Notification} className="iconImg" alt="" />
-                    <div className="counter">2</div>
+                    <div className="counter">{notifications.length}</div>
                 </div>
                 <div className="icon" onClick={()=>setOpen(!open)} >
                     <img src={Message} className="iconImg" alt="" />
-                    <div className="counter">2</div>
+                    
                 </div>
                 <div className="icon" onClick={()=>setOpen(!open)} >
                     <img src={Settings} className="iconImg" alt="" />
-                    <div className="counter">2</div>
+                    
                 </div>
             </div>
             {open && (
             <div className="notifications">
                 {notifications.map((n)=>displayNotification(n))}
+                <button className="nButton">Mark as read</button>
             </div>
             )}
         </div>
